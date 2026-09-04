c5fdfa64224f3227f890453b8a9b59ded7ab04d5
ShaneishaBryant <shaneisharichardson@student.bpcc.edu>
2026-09-03 19:32:55 -0500

    Phase 2 Complete
    Steps I took to complete phase 2:
    > created a function
    > defined a local variable
    > return value
    > called function and print to console
    > attempted to access local value outside the function

--- journal.md
+++ journal.md
@@ -2,3 +2,9 @@ Explain the syntax difference between the function declaration (calculatePrice) 
 
 
 The calculatePrice function works as a standalone statement and begins with the keyword "function". The getTax function is defined inside the expression is assigned to a variable and starts with the variable keyword "let". 
+
+
+
+What happened when you tried to log defaultTaxRate outside the function? Why did that occur?
+
+I received an Uncaught ReferenceError: defaultTaxRate is not defined. This occurs because defaultTaxRate is locally scoped within the calculateStoreTax function. As a result, it only exists while the function is executing and cannot be accessed outside its block.
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Phase 1
-//function declaration
+//create function declaration
 function calculatePrice(price, quantity){
     return price * quantity; 
 }
-//function expression
+//create function expression
 let getTax = function(amount, taxRate){
     return amount * taxRate;
 };
@@ -14,3 +14,12 @@ let tax = getTax(subtotal, 0.07);
 console.log("Subtotal: " + subtotal);
 console.log("Calculated Tax: " + tax);
 
+//Phase 2
+//create function declaration 
+function calculateStoreTax(amount){
+    const defaultTaxRate = 0.07; //local variable 
+    return getTax(amount, defaultTaxRate);
+}
+//call function and pass value
+console.log(calculateStoreTax(100));
+//console.log(defaultTaxRate);
